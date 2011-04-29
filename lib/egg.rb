b383a0eff71fb1a244d94dc502fa47fdbd08fb74
theorician <theorician@me.com>
2011-04-29 02:05:15 +0100

    slight re-write to bring it in to a similar format to that used by https://github.com/runpaint/natwest

--- lib/egg.rb
+++ lib/egg.rb
@@ -1,48 +1,62 @@
 require 'rubygems'
 require 'mechanize'
 
-class Egg
-  attr_accessor :page
-  LOGIN_URL = 'https://your.egg.com/security/customer/login.aspx'
+module Egg
+  URL = 'https://your.egg.com/security/customer/login.aspx'
 
-  def initialize
-    @ua = Mechanize.new {|ua| ua.user_agent_alias = 'Windows IE 7'}
-  end
+  module Login
+    attr_reader :ua
+    attr_accessor :first_name, :last_name, :dob_day, :dob_month, :dob_year, :postcode, :mmn, :password
 
-  def login
-    credentials = YAML.load(File.read(File.expand_path("~/.accounts.yml"))) rescue {}
+    def logged_in?
+      @logged_in ||= false
+    end
 
-    login_form = @ua.get(LOGIN_URL).forms.first
-    login_form['firstName'] = credentials[:egg][:first_name]
-    login_form['lastName']  = credentials[:egg][:last_name]
-    login_form['dobDay']    = credentials[:egg][:dob_day]
-    login_form['dobMonth']  = credentials[:egg][:dob_month]
-    login_form['dobYear']   = credentials[:egg][:dob_year]
-    login_form['postcode']  = credentials[:egg][:postcode]
-    login_form['mmn']       = credentials[:egg][:mmn]
-    login_form['password']  = credentials[:egg][:password]
-    self.page = login_form.submit
-  end
-  
-  def account_balance
-    accounts = self.page.parser.xpath('//table[@id="tblAccounts"]//tr[@class="rowVisible"]')
-    accounts.map do |account|
-      negative = account.xpath('//td[@class="negativebalance money"]').children.first ? account.xpath('//td[@class="negativebalance money"]').children.first.text.match(/[0-9.]+/)[0] : ""
-      positive = account.xpath('//td[@class="positivebalance money"]').children.first ? account.xpath('//td[@class="positivebalance money"]').children.first.text.match(/[0-9.]+/)[0] : ""
-      {
-        :account_name     => account.xpath('//td[@class="accountname"]').children.first.text.strip,
-        :account_type     => account.xpath('//td[@class="accounttype"]').children.first.text.strip,
-        :negative_balance => negative,
-        :positive_balance => positive
-      }
+    def login credentials
+      credentials.each_pair{|name, value| send("#{name}=".to_sym, value)}
+      login_form = @ua.get(URL).forms.first
+      login_form['firstName'] = first_name
+      login_form['lastName']  = last_name
+      login_form['dobDay']    = dob_day
+      login_form['dobMonth']  = dob_month
+      login_form['dobYear']   = dob_year
+      login_form['postcode']  = postcode
+      login_form['mmn']       = mmn
+      login_form['password']  = password
+      self.page = login_form.submit
+      @logged_in = true
     end
   end
-end
 
-egg = Egg.new
-egg.login
-accounts = egg.account_balance
-accounts.each do |acc|
-  print acc[:account_name] + "\t-" + acc[:negative_balance] + "\n"
-end
+
+  class Customer
+    include Login
+    NO_DETAILS = 'No further transaction details held'
+    attr_accessor :page
+
+    def initialize
+      @ua = Mechanize.new {|ua| ua.user_agent_alias = 'Windows IE 7'}
+    end
   
+    def accounts
+      accounts = []
+      page.parser.xpath('//table[@id="tblAccounts"]//tr[@class="rowVisible"]').each do |table|
+        accounts << Account.new.tap do |acc|
+          if table.xpath('//td[@class="negativebalance money"]').children.first
+            acc.balance = table.xpath('//td[@class="negativebalance money"]').children.first.text.match(/[0-9.]+/)[0]
+          end
+          if table.xpath('//td[@class="positivebalance money"]').children.first
+            acc.available = table.xpath('//td[@class="positivebalance money"]').children.first.text.match(/[0-9.]+/)[0]
+          end
+          acc.name = table.xpath('//td[@class="accountname"]').children.first.text.strip
+          acc.number = table.xpath('//td[@class="accounttype"]').children.first.text.strip
+        end
+      end
+      accounts
+    end
+  end
+  
+  class Account 
+    attr_accessor :name, :number, :sort_code, :balance, :available, :transactions
+  end
+end
